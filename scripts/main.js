afd7bf7137d95f61149ce114381ee1dc46d865af
SixtusTheSixth <48032320+SixtusTheSixth@users.noreply.github.com>
2020-12-12 13:50:02 -0500

    Create main.js

---
+++ scripts/main.js
@@ -0,0 +1,34 @@
+
+var spotifyApi = new SpotifyWebApi();
+spotifyApi.setAccessToken('BQBHYe_cDT0TqrPrRt0ULGkgnk9BOkjM7tw6aswS4hPCn0TiTNxusRakhq_rcp44pfUf8RpI58GdMmO6xv2QbHlonzyk3z30dUpup2LbW3SmJQfaZWR6uerzMJ-eMzdWwtExz9A');
+
+function search() {
+    spotifyApi.setAccessToken('BQBHYe_cDT0TqrPrRt0ULGkgnk9BOkjM7tw6aswS4hPCn0TiTNxusRakhq_rcp44pfUf8RpI58GdMmO6xv2QbHlonzyk3z30dUpup2LbW3SmJQfaZWR6uerzMJ-eMzdWwtExz9A');
+    var form = document.getElementById("form");
+    var text = form.elements[0].value;
+    console.log(text);
+    var p_songid = document.getElementById("song_id");
+
+    spotifyApi.searchTracks(text).then(
+        function (data) {
+            console.log(text+" track data", data);
+            track_id = data.tracks.items[0].id;
+            console.log(track_id);
+            p_songid.innerHTML = track_id;
+            return track_id;
+        },
+        function (err) {
+            console.error(err);
+        }
+    ).then(function (track_id) {
+        spotifyApi.getAudioFeaturesForTrack(track_id).then(
+            function (data) {
+                console.log(text+" track  features", data);
+                p_songid.innerHTML = JSON.stringify(data, null, 2);
+            },
+            function (err) {
+                console.error(err);
+            }
+        );
+    });
+}
